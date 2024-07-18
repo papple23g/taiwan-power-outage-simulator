769eaf73b01003290edb29ab4094f22f7f018a94
papple23g <papple12g@gmail.com>
2024-07-18 23:03:27 +0800

    docs:📝update zbpack,json

--- demo/_demo_get_city_name_list.py
+++ demo/_demo_get_city_name_list.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 
-geojson_path = Path(__file__).parent/"twCounty2010merge.geojson"
+geojson_path = Path(__file__).parent.parent/"data/twCounty2010merge.geojson"
 geojson_dict = json.load(geojson_path.open('r', encoding='utf-8'))
 city_name_list = [
     city_dict["properties"]["COUNTYNAME"]
--- demo/_demo_news.py
+++ demo/_demo_news.py
@@ -1,3 +1,7 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))  # noqa
 from libs.news import NEWS_LIST_JSON_PATH, NewsList
 from libs.reason_emoji import get_reason_emoji
 
